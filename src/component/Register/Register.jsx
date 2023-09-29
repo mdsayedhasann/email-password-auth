6006a3132eaace1674387624ba2f3f13ab975f2d
sayed_hossain <sayed.hasan0011@gmail.com>
2023-09-30 00:42:01 +0600

    Set all the Route and make Register page with a Daisy Ui Register  Form

--- src/component/Register/Register.jsx
+++ src/component/Register/Register.jsx
@@ -1,11 +1,53 @@
-import React from 'react';
+import React from "react";
 
 const Register = () => {
-    return (
-        <div>
-            <h1>I am register</h1>
+  return (
+    <div>
+      <div className="hero min-h-screen">
+        <div className="hero-content flex-col lg:flex-row-reverse gap-14">
+          <div className="text-center lg:text-left">
+            <h1 className="text-5xl font-bold">Register now!</h1>
+            <p className="py-6">
+              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
+              excepturi exercitationem quasi. In deleniti eaque aut repudiandae
+              et a id nisi.
+            </p>
+          </div>
+          <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+            <form>
+            <div className="card-body">
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Email</span>
+                </label>
+                <input
+                  type="text"
+                  placeholder="email"
+                  className="input input-bordered"
+                />
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Password</span>
+                </label>
+                <input
+                  type="text"
+                  placeholder="password"
+                  className="input input-bordered"
+                />
+                <label className="label">
+                  <a href="#" className="label-text-alt link link-hover">
+                    Forgot password?
+                  </a>
+                </label>
+              </div>
+            </div>
+            </form>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Register;
---
+++ src/firebase/firebase.init.js
@@ -0,0 +1,18 @@
+
+import { initializeApp , getAuth } from "firebase/app";
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDoZjpJaCQFgZ3vTep1EB_Xec7GFj4XKUs",
+  authDomain: "email-password-auth-98bcc.firebaseapp.com",
+  projectId: "email-password-auth-98bcc",
+  storageBucket: "email-password-auth-98bcc.appspot.com",
+  messagingSenderId: "1014783904867",
+  appId: "1:1014783904867:web:2b7b8aba48f850e6d1bc56"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+export default auth
